058f049cb16601f711a8452f7e6331fdda5e1ed1
Miguel Angel Naranjo <miguelangelnaranjo@talos.local>
2021-02-05 00:09:01 -0500

    Add infinity scroll of pokemons

--- src/features/PokemonList/index.js
+++ src/features/PokemonList/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef, useCallback} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import Pokemon from "./Pokemon";
 import styles from "./PokemonList.module.css"
@@ -6,18 +6,53 @@ import {fetchPokemons} from "./actions";
 
 export function PokemonList() {
 
-    const { pokemonsFiltered } = useSelector(state => state.pokemon)
+    const { page, isLoading, pokemonsFiltered, filtering } = useSelector(state => state.pokemon)
     const dispatch = useDispatch();
 
+    const scrollRef = useRef(null);
+    const actualPage = useRef({})
+
+    actualPage.current = page
+
+    const handleScroll = useCallback((entities, observer) => {
+        const target = entities[0];
+        if (target.isIntersecting && !isLoading && !filtering) {
+            dispatch(fetchPokemons(actualPage.current))
+        }
+    }, [dispatch, isLoading, filtering])
+
     useEffect(() => {
-        dispatch(fetchPokemons(0))
-    }, [dispatch])
+
+        let options = {
+            root: null,
+            rootMargin: "30px",
+            threshold: 1.0
+        }
+
+        let observer = new IntersectionObserver(
+            handleScroll,
+            options
+        );
+
+        observer.observe(scrollRef.current);
+
+        return (() => {
+            observer.disconnect()
+        })
+    }, [dispatch, handleScroll])
+
+    const loadingStyle = { display: isLoading ? "block" : "none"};
 
     return (
-        <div className={styles.pokemonList}>
-            { pokemonsFiltered.map( pokemon => (
-                <Pokemon key={pokemon.name} image={pokemon.url.match(/\d+/g)[1]} name={pokemon.name}/>
-            )) }
+        <div>
+            <div className={styles.pokemonList}>
+                { pokemonsFiltered.map( (pokemon, index) => (
+                    <Pokemon key={index} image={pokemon.url.match(/\d+/g)[1]} name={pokemon.name}/>
+                )) }
+            </div>
+            <div ref={scrollRef}>
+                <span style={loadingStyle}>Loading...</span>
+            </div>
         </div>
     )
 }
--- src/features/PokemonList/reducers.js
+++ src/features/PokemonList/reducers.js
@@ -3,16 +3,30 @@ import {fetchPokemons, filterPokemons} from "./actions";
 
 const initialState = {
     pokemons: [],
-    pokemonsFiltered: []
+    pokemonsFiltered: [],
+    prevY: 0,
+    page: 0,
+    isLoading: false,
+    filtering: false,
 }
 
 const pokemonReducer = createReducer(initialState, (builder) => {
     builder
         .addCase(fetchPokemons.fulfilled, (state, action) => {
-            state.pokemons = action.payload
-            state.pokemonsFiltered = action.payload
+            state.pokemons = [...new Set([...state.pokemons, ...action.payload])];
+            state.pokemonsFiltered = state.pokemons;
+            state.isLoading = false;
+            state.page += 1;
+        })
+        .addCase(fetchPokemons.pending, ( state, actions ) => {
+            state.isLoading = true;
         })
         .addCase(filterPokemons, (state, action) => {
+            if (action.payload.filterBy === null) {
+                state.filtering = false;
+            } else {
+                state.filtering = true;
+            }
             state.pokemonsFiltered = state.pokemons.filter((pokemon) => pokemon.name.includes(action.payload.filterBy))
         })
 })
